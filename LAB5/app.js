d867e11505ab7e35432e5f2a51c5a17111877cc5
Archit <archit7307@gmail.com>
2026-09-17 14:00:07 +0530

    twenty fifth commit

--- LAB5/app.js
+++ LAB5/app.js
@@ -5,7 +5,30 @@ app.get("/about ", (req, res) => {
     res.send("<h1>Hello, Express</h1>");
 });
 
-app.use("*" , (req, res) => {
-    res.send("Route not found ")    ;
+
+// app.listen(3000, () => console.log("Server is running on port 3000"));
+
+app.get('/about' , (req, res) => {
+    res.send("<h1>We are FSD developer</h1>");
 }) ;
-app.listen(3000, () => console.log("Server is running on port 3000"));
+
+
+
+
+app.post('/login' , (req, res) => {
+    res.send({msg : 'user logged in successfully'}) ;
+}) ;
+
+app.put('/update ' , (res,req) =>{
+    res.send({msg : 'user  update successfully'}) ;
+}) ;
+
+app.delete('/user/delete/1' , (req,res) => {
+    res.send({msg : 'user deleted successfully'}) ;
+}) ;
+
+app.use((req,res)=>{
+    res.send("Not found")
+})
+
+app.listen(3333 , () => console.log("Server is running on port 3333"));
